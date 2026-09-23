058531b420abcf95ca84496c96ed40284ba1c24f
Bhavesh Bhole <bhaveshbhole663@gmail.com>
2026-09-23 16:49:11 +0530

    09

---
+++ 09_Student_sort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int marks[5];
+
+ cout<<"Enter Marks of 5 students:";
+ for(int i=0;i<5;i++)
+ {
+  cin>>marks[i];
+ }
+
+ for(int i=0;i<4;i++)
+ {
+  for(int j=0;j<4-i;j++)
+  {
+   if(marks[j]<marks[j+1])
+    {
+     int temp= marks[j];
+     marks[j]=marks[j+1];
+     marks[j+1]=temp;
+    }
+  }
+ }
+ cout<<"\nMarks after arranging:";
+ for(int i=0;i<5;i++)
+ {
+ cout<<marks[i]<<endl;
+ }
+return 0;
+}
